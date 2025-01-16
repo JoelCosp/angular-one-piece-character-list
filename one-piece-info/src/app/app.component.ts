b11477f8bf1b4c467d82cad70b9f37865d83dd88
JoelVRV <145133218+JoelDAW2@users.noreply.github.com>
2025-01-16 13:15:07 +0100

    EMPEZADO EL DISEÑO DE LA PAGINA + DECLARACION DE LAS SECCIONES QUE TENDRA LA MISMA

--- one-piece-info/src/app/app.component.ts
+++ one-piece-info/src/app/app.component.ts
@@ -3,9 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
+// Componentes
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ContainerComponent } from './components/container/container.component';
+
+
+const GLOBAL_MATERIALS = [RouterOutlet, HeaderComponent, FooterComponent, BannerComponent, ContainerComponent];
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GLOBAL_MATERIALS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ one-piece-info/src/app/components/container/container.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  imports: [],
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+  
+}
+
+const guide_sections = [
+  { "name": "Sagas", "url": "", },
+  { "name": "Arcos", "url": "", },
+  { "name": "Frutas del diablo", "url": "", },
+  { "name": "Diales", "url": "", },
+  { "name": "Películas", "url": "", },
+  { "name": "Espadas", "url": "", },
+  { "name": "Hakis", "url": "", },
+  { "name": "Marchas de Luffy", "url": "", },
+  { "name": "Tecnicas de Luffy", "url": "", },
+  { "name": "Tripulaciones", "url": "", },
+  { "name": "Personajes", "url": "", },
+  { "name": "Barcos", "url": "", },
+  { "name": "Available", "url": "", },
+  { "name": "Localizaciones", "url": "", }
+];
